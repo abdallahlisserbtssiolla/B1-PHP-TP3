1eb90fb8910cfecb0cce7be59c00e60876668884
Abdallah Lisser BTS SIO LLA <144156904+abdallahlisserbtssiolla@users.noreply.github.com>
2023-10-04 14:13:43 +0200

    Add files via upload

---
+++ vitesseCroisiere.php
@@ -0,0 +1,14 @@
+<?php
+echo "Saisir la vitesse : \n";
+
+$vitesseSaisie = rtrim(fgets(STDIN));
+
+
+if ($vitesseSaisie < 0) {
+    $vitesseCorrigee = 0;
+} else {
+    $vitesseCorrigee = $vitesseSaisie;
+}
+
+echo "Vitesse :", $vitesseCorrigee, " noeuds\n";
+?>
