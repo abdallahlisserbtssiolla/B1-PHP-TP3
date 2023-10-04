2ac7f994d87ab5b9c1eb58808cfac2ccb3b7f08f
Abdallah Lisser BTS SIO LLA <144156904+abdallahlisserbtssiolla@users.noreply.github.com>
2023-10-04 13:40:33 +0200

    Add files via upload

---
+++ consosPassager.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Saisir la somme due par le passager:";
+$sommeDue = rtrim( fgets( STDIN ));
+
+if( $sommeDue <= 0){
+	echo "Le passager s'est acquitté de la somme due.\n";
+}
+else {
+	echo "Le passager doit",  $sommeDue, " euros.\n";
+}
+
+?>
